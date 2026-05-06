60c00f241d868d69b12b12730141bf30f2bc7e4c
Stefan Derian <stefan.derian@gmail.com>
2026-05-06 18:11:16 +0700

    s3 enablement

--- infrastructure/localstack/init.sh
+++ infrastructure/localstack/init.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 awslocal sqs create-queue --queue-name pokemon-processing --region us-east-1
 echo "SQS queue ready"
+
+awslocal s3 mb s3://raw --region us-east-1                                                                                                                                                                                        
+awslocal s3 mb s3://annotated --region us-east-1                                                                                                                                                                                         
+echo "S3 buckets ready" 
